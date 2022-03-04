8b62ebda72da86de5d589b3ec7030ca987eafe8e
Justin Morris <jnatmorris@gmail.com>
2022-03-04 11:22:18 +0100

    Clean up

--- pages/singlePlayer.tsx
+++ pages/singlePlayer.tsx
@@ -2,340 +2,70 @@ import * as React from "react";
 import type { NextPage } from "next";
 import { AnimatePresence, motion } from "framer-motion";
 import FlagGameText from "../components/FlagGameText";
+import { chooseCountry, countries } from "../components/FlagInfo/FlagInfo";
 
 const Game: NextPage = () => {
-    interface WrongState {
-        wrongNumber: number;
-        currentQuestion: string;
-    }
-
     const [score, setScore] = React.useState<number>(0);
-    const [wrongScore, setWrongScore] = React.useState<WrongState>({
-        wrongNumber: 0,
-        currentQuestion: "",
-    });
+    const [wrongQuestion, setWrongQuestion] = React.useState<string>("");
     const [current, setCurrent] = React.useState<string[]>([""]);
-    const [question, setQuestion] = React.useState<string>("");
+    const [question, setQuestion] = React.useState<string>("AF");
     const [time, setTime] = React.useState<string>("");
-    const [start, setStart] = React.useState(new Date());
+    const [start, setStart] = React.useState<Date>(new Date());
     const [recentWrong, setRecentWrong] = React.useState<boolean>(false);
+    const [nextQuestion, setNextQuestion] = React.useState<number>(0);
 
-    React.useEffect(() => {
-        setInterval(() => {
-            const dateObject = new Date(new Date().getTime() - start.getTime());
-
-            const minute = dateObject.getMinutes();
-            const second = dateObject.getSeconds();
-
-            const currentTime =
-                (minute < 10 ? "0" + minute : minute) +
-                ":" +
-                (second < 10 ? "0" + second : second);
-
-            setTime(currentTime);
-        }, 1000);
-    }, []);
-
+    // on first run, set date (start time to be calculated)
     React.useEffect(() => {
         setStart(new Date());
     }, []);
 
-    interface items {
-        [key: string]: string;
-    }
-    // country codes and names
-    const countries: items = {
-        AF: "Afghanistan",
-        AX: "Åland Islands",
-        AL: "Albania",
-        DZ: "Algeria",
-        AS: "American Samoa",
-        AD: "Andorra",
-        AO: "Angola",
-        AI: "Anguilla",
-        AQ: "Antarctica",
-        AG: "Antigua and Barbuda",
-        AR: "Argentina",
-        AM: "Armenia",
-        AW: "Aruba",
-        AU: "Australia",
-        AT: "Austria",
-        AZ: "Azerbaijan",
-        BS: "Bahamas",
-        BH: "Bahrain",
-        BD: "Bangladesh",
-        BB: "Barbados",
-        BY: "Belarus",
-        BE: "Belgium",
-        BZ: "Belize",
-        BJ: "Benin",
-        BM: "Bermuda",
-        BT: "Bhutan",
-        BO: "Bolivia (Plurinational State of)",
-        BQ: '"Bonaire',
-        BA: "Bosnia and Herzegovina",
-        BW: "Botswana",
-        BV: "Bouvet Island",
-        BR: "Brazil",
-        IO: "British Indian Ocean Territory",
-        BN: "Brunei Darussalam",
-        BG: "Bulgaria",
-        BF: "Burkina Faso",
-        BI: "Burundi",
-        CV: "Cabo Verde",
-        KH: "Cambodia",
-        CM: "Cameroon",
-        CA: "Canada",
-        KY: "Cayman Islands",
-        CF: "Central African Republic",
-        TD: "Chad",
-        CL: "Chile",
-        CN: "China",
-        CX: "Christmas Island",
-        CC: "Cocos (Keeling) Islands",
-        CO: "Colombia",
-        KM: "Comoros",
-        CG: "Congo",
-        CD: '"Congo',
-        CK: "Cook Islands",
-        CR: "Costa Rica",
-        CI: "Côte d'Ivoire",
-        HR: "Croatia",
-        CU: "Cuba",
-        CW: "Curaçao",
-        CY: "Cyprus",
-        CZ: "Czechia",
-        DK: "Denmark",
-        DJ: "Djibouti",
-        DM: "Dominica",
-        DO: "Dominican Republic",
-        EC: "Ecuador",
-        EG: "Egypt",
-        SV: "El Salvador",
-        GQ: "Equatorial Guinea",
-        ER: "Eritrea",
-        EE: "Estonia",
-        SZ: "Eswatini",
-        ET: "Ethiopia",
-        FK: "Falkland Islands (Malvinas)",
-        FO: "Faroe Islands",
-        FJ: "Fiji",
-        FI: "Finland",
-        FR: "France",
-        GF: "French Guiana",
-        PF: "French Polynesia",
-        TF: "French Southern Territories",
-        GA: "Gabon",
-        GM: "Gambia",
-        GE: "Georgia",
-        DE: "Germany",
-        GH: "Ghana",
-        GI: "Gibraltar",
-        GR: "Greece",
-        GL: "Greenland",
-        GD: "Grenada",
-        GP: "Guadeloupe",
-        GU: "Guam",
-        GT: "Guatemala",
-        GG: "Guernsey",
-        GN: "Guinea",
-        GW: "Guinea-Bissau",
-        GY: "Guyana",
-        HT: "Haiti",
-        HM: "Heard Island and McDonald Islands",
-        VA: "Holy See",
-        HN: "Honduras",
-        HK: "Hong Kong",
-        HU: "Hungary",
-        IS: "Iceland",
-        IN: "India",
-        ID: "Indonesia",
-        IR: "Iran (Islamic Republic of)",
-        IQ: "Iraq",
-        IE: "Ireland",
-        IM: "Isle of Man",
-        IL: "Israel",
-        IT: "Italy",
-        JM: "Jamaica",
-        JP: "Japan",
-        JE: "Jersey",
-        JO: "Jordan",
-        KZ: "Kazakhstan",
-        KE: "Kenya",
-        KI: "Kiribati",
-        KP: "Korea (Democratic People's Republic of)",
-        KR: '"Korea',
-        KW: "Kuwait",
-        KG: "Kyrgyzstan",
-        LA: "Lao People's Democratic Republic",
-        LV: "Latvia",
-        LB: "Lebanon",
-        LS: "Lesotho",
-        LR: "Liberia",
-        LY: "Libya",
-        LI: "Liechtenstein",
-        LT: "Lithuania",
-        LU: "Luxembourg",
-        MO: "Macao",
-        MG: "Madagascar",
-        MW: "Malawi",
-        MY: "Malaysia",
-        MV: "Maldives",
-        ML: "Mali",
-        MT: "Malta",
-        MH: "Marshall Islands",
-        MQ: "Martinique",
-        MR: "Mauritania",
-        MU: "Mauritius",
-        YT: "Mayotte",
-        MX: "Mexico",
-        FM: "Micronesia (Federated States of)",
-        MD: '"Moldova',
-        MC: "Monaco",
-        MN: "Mongolia",
-        ME: "Montenegro",
-        MS: "Montserrat",
-        MA: "Morocco",
-        MZ: "Mozambique",
-        MM: "Myanmar",
-        NA: "Namibia",
-        NR: "Nauru",
-        NP: "Nepal",
-        NL: "Netherlands",
-        NC: "New Caledonia",
-        NZ: "New Zealand",
-        NI: "Nicaragua",
-        NE: "Niger",
-        NG: "Nigeria",
-        NU: "Niue",
-        NF: "Norfolk Island",
-        MK: "North Macedonia",
-        MP: "Northern Mariana Islands",
-        NO: "Norway",
-        OM: "Oman",
-        PK: "Pakistan",
-        PW: "Palau",
-        PS: '"Palestine',
-        PA: "Panama",
-        PG: "Papua New Guinea",
-        PY: "Paraguay",
-        PE: "Peru",
-        PH: "Philippines",
-        PN: "Pitcairn",
-        PL: "Poland",
-        PT: "Portugal",
-        PR: "Puerto Rico",
-        QA: "Qatar",
-        RE: "Réunion",
-        RO: "Romania",
-        RU: "Russian Federation",
-        RW: "Rwanda",
-        BL: "Saint Barthélemy",
-        SH: '"Saint Helena',
-        KN: "Saint Kitts and Nevis",
-        LC: "Saint Lucia",
-        MF: "Saint Martin (French part)",
-        PM: "Saint Pierre and Miquelon",
-        VC: "Saint Vincent and the Grenadines",
-        WS: "Samoa",
-        SM: "San Marino",
-        ST: "Sao Tome and Principe",
-        SA: "Saudi Arabia",
-        SN: "Senegal",
-        RS: "Serbia",
-        SC: "Seychelles",
-        SL: "Sierra Leone",
-        SG: "Singapore",
-        SX: "Sint Maarten (Dutch part)",
-        SK: "Slovakia",
-        SI: "Slovenia",
-        SB: "Solomon Islands",
-        SO: "Somalia",
-        ZA: "South Africa",
-        GS: "South Georgia and the South Sandwich Islands",
-        SS: "South Sudan",
-        ES: "Spain",
-        LK: "Sri Lanka",
-        SD: "Sudan",
-        SR: "Suriname",
-        SJ: "Svalbard and Jan Mayen",
-        SE: "Sweden",
-        CH: "Switzerland",
-        SY: "Syrian Arab Republic",
-        TW: '"Taiwan',
-        TJ: "Tajikistan",
-        TZ: '"Tanzania',
-        TH: "Thailand",
-        TL: "Timor-Leste",
-        TG: "Togo",
-        TK: "Tokelau",
-        TO: "Tonga",
-        TT: "Trinidad and Tobago",
-        TN: "Tunisia",
-        TR: "Turkey",
-        TM: "Turkmenistan",
-        TC: "Turks and Caicos Islands",
-        TV: "Tuvalu",
-        UG: "Uganda",
-        UA: "Ukraine",
-        AE: "United Arab Emirates",
-        GB: "United Kingdom of Great Britain and Northern Ireland",
-        US: "United States of America",
-        UM: "United States Minor Outlying Islands",
-        UY: "Uruguay",
-        UZ: "Uzbekistan",
-        VU: "Vanuatu",
-        VE: "Venezuela (Bolivarian Republic of)",
-        VN: "Viet Nam",
-        VG: "Virgin Islands (British)",
-        VI: "Virgin Islands (U.S.)",
-        WF: "Wallis and Futuna",
-        EH: "Western Sahara",
-        YE: "Yemen",
-        ZM: "Zambia",
-        ZW: "Zimbabwe",
-    };
+    React.useEffect(() => {
+        setInterval(() => {
+            // calculate time based on Date (more accurate than setInterval )
+            const dateObject = new Date(new Date().getTime() - start.getTime());
 
-    const scoreHandler = (item: string): void => {
-        if (item === question) {
-            setRecentWrong(false);
-            setScore(score + 1);
-        } else {
-            setRecentWrong(true);
-            if (question === wrongScore.currentQuestion) return;
-            setWrongScore({
-                wrongNumber: wrongScore.wrongNumber + 1,
-                currentQuestion: question,
-            });
-        }
-    };
+            // Calculate minutes, seconds, and configure string that's set
+            const minute = dateObject.getMinutes();
+            const second = dateObject.getSeconds();
+            setTime(
+                (minute < 10 ? "0" + minute : minute) +
+                    ":" +
+                    (second < 10 ? "0" + second : second)
+            );
+        }, 1000);
+    }, []);
 
-    const chooseCountry = (): string[] => {
-        var countryA, countryB;
-
-        // run at least once or as long as countries are same
-        do {
-            countryA =
-                Object.keys(countries)[
-                    (Object.keys(countries).length * Math.random()) << 0
-                ];
-
-            countryB =
-                Object.keys(countries)[
-                    (Object.keys(countries).length * Math.random()) << 0
-                ];
-        } while (countryA === countryB);
-
-        return [countryA, countryB];
-    };
-
+    // when next question changes, pick new flags
     React.useEffect(() => {
         const [countryA, countryB] = chooseCountry();
 
+        // pick random number between 0 and 1 to determine which flag is the question
         setQuestion(Math.random() > 0.5 ? countryA : countryB);
 
+        // the current flags to be shown
         setCurrent([countryA, countryB]);
-    }, [score]);
+    }, [nextQuestion]);
+
+    const scoreHandler = (item: string): void => {
+        // if flag clicked is correct
+        if (item === question) {
+            // remove text below flags
+            setRecentWrong(false);
+
+            // add to score if answer isn't shown on screen
+            if (wrongQuestion !== question) setScore(score + 1);
+
+            // once correct flag clicked, set next question
+            setNextQuestion(nextQuestion + 1);
+        } else {
+            // if flag clicked is wrong, show text
+            setRecentWrong(true);
+
+            // if already set, don't reset
+            if (question === wrongQuestion) return;
+            setWrongQuestion(question);
+        }
+    };
 
     return (
         <div className="prose dark:prose-invert prose-p:m-0 prose-h3:m-0 prose-h2:m-0 prose-h4:m-0 mx-2 mt-[10vh] rounded-md py-2">
@@ -354,7 +84,7 @@ const Game: NextPage = () => {
                         </p>
                     </div>
                     <h3 className="px-2 pt-4 pb-2 text-center break-words">
-                        Which is {countries[question]}?
+                        Which is {countries[question].replace(/['"]+/g, "")}?
                     </h3>
                     <div className="grid  h-fit grid-cols-2 place-items-center gap-x-2  px-1.5">
                         {/* not using next images to allow for auto sizing */}
@@ -393,7 +123,10 @@ const Game: NextPage = () => {
                                                     : "text-center text-red-400 dark:text-red-600"
                                             }
                                         >
-                                            {countries[current[0]]}
+                                            {countries[current[0]].replace(
+                                                /['"]+/g,
+                                                ""
+                                            )}
                                         </h3>
 
                                         <h3
@@ -403,7 +136,10 @@ const Game: NextPage = () => {
                                                     : "text-center text-red-400 dark:text-red-600"
                                             }
                                         >
-                                            {countries[current[1]]}
+                                            {countries[current[1]].replace(
+                                                /['"]+/g,
+                                                ""
+                                            )}
                                         </h3>
                                     </div>
                                 </motion.div>
