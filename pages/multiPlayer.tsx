7cb2c7a433f5ccbdecb398794eefc6a2504b70f5
Justin Morris <jnatmorris@gmail.com>
2022-03-05 11:55:45 +0100

    added comments

--- pages/multiPlayer.tsx
+++ pages/multiPlayer.tsx
@@ -4,12 +4,15 @@ import ChoosePlayer from "../components/Multiplayer/ChoosePlayer";
 import ChooseName from "../components/Multiplayer/chooseName";
 
 const MultiPlayer: React.FC = () => {
+    // state to track what to render
     const [hasChoosenName, setHasChooseName] = React.useState<boolean>(false);
     return (
+        // consume socket info passed down
         <SocketInfo.Consumer>
             {({ myInfo, playersReady, HandleSetName, handleSendName }) => (
                 <>
                     {!hasChoosenName ? (
+                        // render when user needs to choose name
                         <ChooseName
                             HandleSetName={HandleSetName}
                             myInfo={myInfo}
@@ -17,6 +20,7 @@ const MultiPlayer: React.FC = () => {
                             handleSendName={handleSendName}
                         />
                     ) : (
+                        // render when user has choosen their name
                         <ChoosePlayer
                             playersReady={playersReady}
                             myInfo={myInfo}
