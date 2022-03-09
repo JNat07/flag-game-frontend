9ffa71216edc100356f0a8449d5bd8d187f24b06
Justin Morris <jnatmorris@gmail.com>
2022-03-09 22:12:12 +0100

    Added random info

---
+++ components/randomInfo.ts
@@ -0,0 +1,23 @@
+// need to find a better place for this
+const names: string[] = [
+    "Olivia",
+    "Emma",
+    "Ava",
+    "Charlotte",
+    "Sophia",
+    "Liam",
+    "Noah",
+    "Oliver",
+    "Elijah",
+    "Mary",
+    "Dorothy",
+    "Helen",
+    "Margaret",
+    "John",
+    "Robert",
+    "William",
+    "James",
+    "Charles",
+];
+
+export { names };
