0df2e37e44f615ea166d5f5f3fbda8ef6b60e92e
Justin Morris <jnatmorris@gmail.com>
2022-03-15 22:31:39 +0100

    Cleanup

--- pages/singlePlayer.tsx
+++ pages/singlePlayer.tsx
@@ -1,19 +1,7 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Game from "../components/game";
-import { SocketContext } from "./_app";
 
-const SinglePlayer: NextPage = () => {
-    return (
-        <SocketContext.Consumer>
-            {({ myInfo, playersReady, setConnect, HandleSetName }) => (
-                <Game
-                    singlePlayer={true}
-                    multiplayerGameInfo={["temp", "temp", "temp"]}
-                />
-            )}
-        </SocketContext.Consumer>
-    );
-};
+const SinglePlayer: NextPage = () => <Game singlePlayer={true} />;
 
 export default SinglePlayer;
