9010ff2b44511ef076e797dcdade77fc630a8249
Justin Morris <jnatmorris@gmail.com>
2022-03-22 14:10:35 +0100

    Moved offline detector

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import CheckOnline from "../components/OnlineChecker";
 import Menu from "../components/Index/Menu";
 
 const Home: NextPage = () => {
-    // custom offline hook
-    if (!CheckOnline()) alert("offline");
-
     return (
         <main className="bg-white dark:bg-black">
             {/* inject elem to head of page */}
