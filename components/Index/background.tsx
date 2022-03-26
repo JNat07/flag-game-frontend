1db221c05fe3a8775ff2fe8f8dada9bb3d32e390
Justin Morris <jnatmorris@gmail.com>
2022-03-26 14:34:53 +0100

    add flags && change shadow

--- components/Index/background.tsx
+++ components/Index/background.tsx
@@ -18,74 +18,74 @@ const Background: React.FC = () => {
     };
 
     return (
-        <div className="absolute w-screen space-y-5 overflow-hidden blur-[5px]">
+        <div className="fixed top-2 left-0 w-screen space-y-7 overflow-hidden blur-[5px]">
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateLeft"}
                 src="./MenuFlags/de.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="Germany_Flag"
             />
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateLeft"}
                 src="./MenuFlags/cm.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="Cameroon_Flag"
             />
 
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateRight"}
                 src="./MenuFlags/vc.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
-                alt="Country_Flag"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
+                alt="Saint_Vincent_And_The_Grenadines_Flag"
             />
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateLeft"}
                 src="./MenuFlags/hk.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(7, 17),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="Hong_Kong_Flag"
             />
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateLeft"}
                 src="./MenuFlags/bd.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
 
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="Bangladesh_Flag"
             />
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateRight"}
                 src="./MenuFlags/cl.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 alt="Country_Flag"
                 transition={{
                     duration: RandomNumber(16, 20),
@@ -97,38 +97,63 @@ const Background: React.FC = () => {
                 variants={imageMoveVarient}
                 animate={"animateRight"}
                 src="./MenuFlags/us.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
-                alt="Country_Flag"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
+                alt="United_States_Of_America_Flag"
+            />
+            <motion.img
+                variants={imageMoveVarient}
+                animate={"animateLeft"}
+                src="./MenuFlags/eg.png"
+                className="w-auto h-16 rounded-md hover:shadow-md"
+                transition={{
+                    duration: RandomNumber(16, 20),
+                    ease: [0.5, 0.5, 0.5, 0.5],
+                    repeat: Infinity,
+                }}
+                alt="Egypt_Flag"
+            />
+
+            <motion.img
+                variants={imageMoveVarient}
+                animate={"animateRight"}
+                src="./MenuFlags/il.png"
+                className="w-auto h-16 rounded-md hover:shadow-md"
+                transition={{
+                    duration: RandomNumber(16, 20),
+                    ease: [0.5, 0.5, 0.5, 0.5],
+                    repeat: Infinity,
+                }}
+                alt="Israel_Flag"
             />
             <motion.img
                 variants={imageMoveVarient}
                 animate={"animateLeft"}
                 src="./MenuFlags/cc.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="The_Cocos_Islands_Flag"
             />
 
             <motion.img
                 variants={imageMoveVarient}
-                animate={"animateLeft"}
+                animate={"animateRight"}
                 src="./MenuFlags/ht.png"
-                className="w-auto h-16 rounded-md hover:shadow-2xl"
+                className="w-auto h-16 rounded-md hover:shadow-md"
                 transition={{
                     duration: RandomNumber(16, 20),
                     ease: [0.5, 0.5, 0.5, 0.5],
                     repeat: Infinity,
                 }}
-                alt="Country_Flag"
+                alt="Haiti_Flag"
             />
         </div>
     );
