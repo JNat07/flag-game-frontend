b57758bcb1fa34a20516191d1a8c353a743366c7
Justin Morris <jnatmorris@gmail.com>
2022-03-27 13:40:04 +0200

    update styling

--- components/FlagGameText.tsx
+++ components/FlagGameText.tsx
@@ -3,13 +3,18 @@ import * as React from "react";
 
 const FlagGameText: React.FC = () => (
     <motion.div
-        className="prose mt-4  rounded-md p-1 px-1.5 ring-1 ring-black prose-h1:m-0 dark:prose-invert dark:ring-white"
-        whileHover={{ scale: 1.5 }}
+        className="prose mt-4 w-fit rounded-md p-1 px-1.5 ring-1 ring-black prose-h1:m-0 dark:prose-invert dark:ring-white "
         whileTap={{ scale: 0.98 }}
     >
-        <h1 className="text-3xl font-medium">The</h1>
-        <h1 className="text-4xl font-semibold">Flag</h1>
-        <h1 className="text-4xl font-black">Game</h1>
+        <h1 className="cursor-pointer text-3xl font-medium  selection:bg-transparent">
+            The
+        </h1>
+        <h1 className="cursor-pointer text-4xl font-semibold selection:bg-transparent">
+            Flag
+        </h1>
+        <h1 className="cursor-pointer text-4xl font-black  selection:bg-transparent">
+            Game
+        </h1>
     </motion.div>
 );
 
