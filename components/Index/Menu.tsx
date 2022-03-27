c295bd9a3475a46a99d082ce2b9c1927d51d873a
Justin Morris <jnatmorris@gmail.com>
2022-03-27 11:02:52 +0200

    cleanup and alter css

--- components/Index/Menu.tsx
+++ components/Index/Menu.tsx
@@ -4,10 +4,9 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { SocketContext } from "../../pages/_app";
 import Background from "./background";
+import { UserIcon, UsersIcon } from "@heroicons/react/solid";
 
 const Menu: NextPage = () => {
-    const [startGame, setStartGame] = React.useState<boolean>(false);
-
     return (
         <div className="overflow-y-hidden">
             <Background />
@@ -15,8 +14,8 @@ const Menu: NextPage = () => {
                 {({ setConnect }) => (
                     <main className="relative z-20 flex">
                         <div className="m-auto">
-                            <div className="prose mt-[17vh] h-fit rounded-lg prose-h1:m-0 dark:prose-invert dark:shadow-none">
-                                <div className="pb-1 mt-12 space-y-7">
+                            <div className="prose mt-[17vh] h-fit rounded-lg prose-h1:m-0 prose-h4:m-0 dark:prose-invert dark:shadow-none">
+                                <div className="mt-12 space-y-7 pb-1">
                                     <div className="flex justify-center ">
                                         <Link
                                             href={"/singlePlayer"}
@@ -26,12 +25,10 @@ const Menu: NextPage = () => {
                                             <motion.a
                                                 whileHover={{ scale: 0.98 }}
                                                 whileTap={{ scale: 0.95 }}
-                                                className="w-full px-16 text-3xl text-center no-underline rounded-md shadow-lg ring ring-black dark:ring-gray-50 dark:hover:ring-white/70"
-                                                onClick={() =>
-                                                    setStartGame(true)
-                                                }
+                                                className="flex w-full items-center space-x-3 rounded-md px-16 pl-12 text-center text-3xl no-underline shadow-lg ring ring-gray-800 hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
                                             >
-                                                Single Player
+                                                <UserIcon className="h-8 w-8" />
+                                                <h4> Single Player</h4>
                                             </motion.a>
                                         </Link>
                                     </div>
@@ -44,13 +41,11 @@ const Menu: NextPage = () => {
                                             <motion.a
                                                 whileHover={{ scale: 0.98 }}
                                                 whileTap={{ scale: 0.95 }}
-                                                className="w-full px-16 text-3xl text-center no-underline rounded-md shadow-lg ring ring-gray-800 hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
-                                                onClick={() => {
-                                                    setConnect(true);
-                                                    setStartGame(true);
-                                                }}
+                                                className="flex w-full items-center space-x-3 rounded-md px-16 pl-12 text-center text-3xl no-underline shadow-lg ring ring-gray-800 hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
+                                                onClick={() => setConnect(true)}
                                             >
-                                                Multi Player
+                                                <UsersIcon className="h-8 w-8" />
+                                                <h4>Multi Player</h4>
                                             </motion.a>
                                         </Link>
                                     </div>
