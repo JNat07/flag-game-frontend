920f213f0e7b8abc2695c2e53513468347b4889e
Justin Morris <jnatmorris@gmail.com>
2022-03-27 13:40:42 +0200

    Update large screen styling

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -18,48 +18,55 @@ const Header: React.FC = () => {
     return (
         <SocketContext.Consumer>
             {({ setConnect }) => (
-                <header className="relative inset-x-0 top-0 z-20 grid grid-cols-3 p-2 prose prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert ">
-                    <ThemeToggle pageLoaded={pageLoaded} />
-                    <div /> {/* middle col empty */}
-                    <div className="flex justify-end">
-                        <AnimatePresence>
-                            {/* only show home button when not on menu */}
-                            {router.pathname === "/" ? (
-                                <motion.a
-                                    href="https://github.com/JNat07"
-                                    whileHover={{ scale: 0.95 }}
-                                    whileTap={{ scale: 0.95 }}
-                                >
-                                    <img
-                                        src="./GitHubLogo.png"
-                                        className="w-10 h-10 dark:invert"
-                                    />
-                                </motion.a>
-                            ) : (
-                                <motion.div
-                                    animate={{ opacity: 1 }}
-                                    exit={{ opacity: 0 }}
-                                    whileTap={{ scale: 0.95 }}
-                                    className="flex space-x-1 text-sm "
-                                    onClick={() =>
-                                        // if on multiPlayer page, set connect false
-                                        router.pathname === "/multiPlayer"
-                                            ? setConnect(false)
-                                            : null
-                                    }
-                                >
-                                    <p className="pt-1 text-center">Exit</p>
-                                    <Link href={"/"} passHref>
-                                        <a>
-                                            <HomeIcon className="w-6 h-6" />
-                                        </a>
-                                    </Link>
-                                </motion.div>
-                            )}
-                        </AnimatePresence>
-                    </div>
-                    <FlagGameText />
-                </header>
+                <div className="m-0">
+                    <header className="prose relative inset-x-0 top-0 z-20 prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
+                        <div className="grid w-screen  grid-cols-3 px-2 py-2 lg:py-4 lg:px-5">
+                            <ThemeToggle pageLoaded={pageLoaded} />
+                            <div /> {/* middle col empty */}
+                            <div className="flex justify-end">
+                                <AnimatePresence>
+                                    {/* only show home button when not on menu */}
+                                    {router.pathname === "/" ? (
+                                        <motion.a
+                                            href="https://github.com/JNat07"
+                                            whileHover={{ scale: 0.95 }}
+                                            whileTap={{ scale: 0.95 }}
+                                        >
+                                            <img
+                                                src="./GitHubLogo.png"
+                                                className="h-10 w-10 dark:invert"
+                                            />
+                                        </motion.a>
+                                    ) : (
+                                        <motion.div
+                                            animate={{ opacity: 1 }}
+                                            exit={{ opacity: 0 }}
+                                            whileTap={{ scale: 0.95 }}
+                                            className="flex space-x-1 text-sm "
+                                            onClick={() =>
+                                                // if on multiPlayer page, set connect false
+                                                router.pathname ===
+                                                "/multiPlayer"
+                                                    ? setConnect(false)
+                                                    : null
+                                            }
+                                        >
+                                            <p className="pt-1 text-center">
+                                                Exit
+                                            </p>
+                                            <Link href={"/"} passHref>
+                                                <a>
+                                                    <HomeIcon className="h-6 w-6" />
+                                                </a>
+                                            </Link>
+                                        </motion.div>
+                                    )}
+                                </AnimatePresence>
+                            </div>
+                            <FlagGameText />
+                        </div>
+                    </header>
+                </div>
             )}
         </SocketContext.Consumer>
     );
