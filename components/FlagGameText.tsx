8fc5d6cda8e0241fbb1f8725a66de6a48f61bf8d
Justin Morris <jnatmorris@gmail.com>
2022-03-29 12:32:38 +0200

    fix css

--- components/FlagGameText.tsx
+++ components/FlagGameText.tsx
@@ -6,13 +6,13 @@ const FlagGameText: React.FC = () => (
         className="prose mt-4 w-fit rounded-md p-1 px-1.5 ring-1 ring-black prose-h1:m-0 dark:prose-invert dark:ring-white "
         whileTap={{ scale: 0.98 }}
     >
-        <h1 className="cursor-pointer text-3xl font-medium  selection:bg-transparent">
+        <h1 className="text-3xl font-medium cursor-pointer select-none ">
             The
         </h1>
-        <h1 className="cursor-pointer text-4xl font-semibold selection:bg-transparent">
+        <h1 className="text-4xl font-semibold cursor-pointer select-none">
             Flag
         </h1>
-        <h1 className="cursor-pointer text-4xl font-black  selection:bg-transparent">
+        <h1 className="text-4xl font-black cursor-pointer select-none ">
             Game
         </h1>
     </motion.div>
