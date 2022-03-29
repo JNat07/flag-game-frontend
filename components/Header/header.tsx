cdfd867edc2124251cbd5826bcd3210ccd6e7554
Justin Morris <jnatmorris@gmail.com>
2022-03-29 14:58:31 +0200

    fixed header && changed github image

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import Link from "next/link";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { HomeIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import ThemeToggle from "../DarkMode/ThemeToggle";
 import FlagGameText from "../FlagGameText";
 import Image from "next/image";
+import ReactMorph from "react-morph";
 
 const Header: React.FC = () => {
     const [pageLoaded, setPageLoaded] = React.useState<boolean>(false);
@@ -17,45 +18,51 @@ const Header: React.FC = () => {
 
     return (
         <div className="m-0">
-            <header className="prose relative inset-x-0 top-0 z-20 prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
+            <header className="relative inset-x-0 top-0 z-20 prose prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
                 <div className="grid w-screen grid-cols-3 px-2 py-2 lg:py-4 lg:px-5">
                     <ThemeToggle pageLoaded={pageLoaded} />
                     <div /> {/* middle col empty */}
                     <div className="flex justify-end">
-                        <AnimatePresence>
-                            {/* only show home button when not on menu */}
-                            {router.pathname === "/" ? (
+                        {/* only show home button when not on menu */}
+                        {router.pathname === "/" ? (
+                            <AnimatePresence>
                                 <motion.a
                                     href="https://github.com/JNat07"
-                                    whileHover={{ scale: 0.95 }}
-                                    whileTap={{ scale: 0.95 }}
+                                    className="-mt-1.5"
                                 >
-                                    <Image
-                                        src="/GitHubLogo.png"
-                                        width={40}
-                                        height={40}
-                                        className="dark:invert"
+                                    <motion.img
+                                        src="../GitHubLogo.png"
+                                        className="h-11 w-fit dark:invert lg:h-12"
                                         alt="someInfo"
+                                        whileHover={{ scale: 0.95 }}
+                                        whileTap={{ scale: 0.95 }}
+                                        exit={{ opacity: 0 }}
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
                                     />
                                 </motion.a>
-                            ) : (
+                            </AnimatePresence>
+                        ) : (
+                            <AnimatePresence>
                                 <motion.div
-                                    animate={{ opacity: 1 }}
-                                    exit={{ opacity: 0 }}
                                     whileTap={{ scale: 0.95 }}
                                     className="flex space-x-1 text-sm "
+                                    exit={{ opacity: 0 }}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
                                 >
                                     <p className="pt-1 text-center">Exit</p>
                                     <Link href={"/"} passHref>
                                         <a>
-                                            <HomeIcon className="h-6 w-6" />
+                                            <HomeIcon className="w-6 h-6" />
                                         </a>
                                     </Link>
                                 </motion.div>
-                            )}
-                        </AnimatePresence>
+                            </AnimatePresence>
+                        )}
                     </div>
                     <FlagGameText />
+                    <div></div>
                 </div>
             </header>
         </div>
