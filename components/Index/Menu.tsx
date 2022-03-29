94f8720151081b5ef34330ccb77d441f31dd5278
Justin Morris <jnatmorris@gmail.com>
2022-03-29 14:58:15 +0200

    added random name button

--- components/Index/Menu.tsx
+++ components/Index/Menu.tsx
@@ -15,7 +15,7 @@ const Menu: NextPage = () => {
             <main className="relative z-20 flex">
                 <div className="m-auto">
                     <div className="prose mt-[17vh] h-fit rounded-lg prose-h1:m-0 prose-h4:m-0 dark:prose-invert dark:shadow-none">
-                        <div className="mt-12 space-y-7 pb-1">
+                        <div className="pb-1 mt-12 space-y-7">
                             <div className="flex justify-center ">
                                 <Link
                                     href={"/singlePlayer"}
@@ -25,9 +25,9 @@ const Menu: NextPage = () => {
                                     <motion.a
                                         whileHover={{ scale: 0.98 }}
                                         whileTap={{ scale: 0.95 }}
-                                        className="flex w-full select-none items-center space-x-3 rounded-md px-16 pl-12 text-center text-3xl no-underline shadow-md ring ring-gray-800 hover:shadow-lg hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
+                                        className="flex items-center w-full px-16 pl-12 space-x-3 text-3xl text-center no-underline rounded-md shadow-md select-none ring ring-gray-800 hover:shadow-lg hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
                                     >
-                                        <UserIcon className="h-8 w-8" />
+                                        <UserIcon className="w-8 h-8" />
                                         <h4> Single Player</h4>
                                     </motion.a>
                                 </Link>
@@ -41,9 +41,9 @@ const Menu: NextPage = () => {
                                     <motion.a
                                         whileHover={{ scale: 0.98 }}
                                         whileTap={{ scale: 0.95 }}
-                                        className="flex w-full select-none items-center space-x-3 rounded-md px-16 pl-12 text-center text-3xl no-underline shadow-md ring ring-gray-800 hover:shadow-lg hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
+                                        className="flex items-center w-full px-16 pl-12 space-x-3 text-3xl text-center no-underline rounded-md shadow-md select-none ring ring-gray-800 hover:shadow-lg hover:ring-black dark:ring-gray-50 dark:hover:ring-white/70"
                                     >
-                                        <UsersIcon className="h-8 w-8" />
+                                        <UsersIcon className="w-8 h-8" />
                                         <h4>Multi Player</h4>
                                     </motion.a>
                                 </Link>
