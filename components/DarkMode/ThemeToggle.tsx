4d13f5d3cdb5e417de8180efb837eeaea2e92d43
Justin Morris <jnatmorris@gmail.com>
2022-03-30 09:14:15 +0200

    Remove theme context

--- components/DarkMode/ThemeToggle.tsx
+++ components/DarkMode/ThemeToggle.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { motion, Variants } from "framer-motion";
 import { GlobeIcon } from "@heroicons/react/outline";
-import { ThemeContext } from "../../pages/_app";
 import { themeToggleType } from "../types";
+import DarkMode from "./DarkMode";
 
 const ThemeToggle: React.FC<themeToggleType> = ({ pageLoaded }) => {
     const themeToggleVar: Variants = {
@@ -27,45 +27,44 @@ const ThemeToggle: React.FC<themeToggleType> = ({ pageLoaded }) => {
     const darkOrLight = (theme: string): string =>
         theme === "dark" ? "left" : "right";
 
+    const { theme, setTheme } = DarkMode();
+
     return (
         /* disable flickering of text */
         pageLoaded ? (
             // consume theme and settheme
-            <ThemeContext.Consumer>
-                {({ theme, setTheme }) => (
-                    <div>
-                        <div
-                            id="globe"
-                            onClick={() =>
-                                setTheme(theme === "light" ? "dark" : "light")
-                            }
-                            className="flex w-12 h-5 rounded-full justify-self-start ring-2 ring-black dark:ring-white "
-                        >
-                            {/* animate the globe icon moving */}
-                            <motion.div
-                                variants={themeToggleVar}
-                                animate={darkOrLight(theme)}
-                                initial={darkOrLight(theme)}
-                                transition={{
-                                    ease: [0.25, 0.5, 0.75, 1],
-                                }}
-                            >
-                                <GlobeIcon className="w-5 h-5 cursor-pointer stroke-black dark:stroke-white" />
-                            </motion.div>
-                        </div>
 
-                        <motion.p
-                            className="text-black cursor-default selection:bg-transparent dark:text-white"
-                            variants={themeLabelVarient}
-                            animate={theme === "light" ? "light" : "dark"}
-                            initial={{ opacity: 1 }}
-                        >
-                            {/*  theme initialized, else know OS preference */}
-                            {theme ? theme : "System"}
-                        </motion.p>
-                    </div>
-                )}
-            </ThemeContext.Consumer>
+            <div>
+                <div
+                    id="globe"
+                    onClick={() =>
+                        setTheme(theme === "light" ? "dark" : "light")
+                    }
+                    className="flex w-12 h-5 rounded-full justify-self-start ring-2 ring-black dark:ring-white "
+                >
+                    {/* animate the globe icon moving */}
+                    <motion.div
+                        variants={themeToggleVar}
+                        animate={darkOrLight(theme)}
+                        initial={darkOrLight(theme)}
+                        transition={{
+                            ease: [0.25, 0.5, 0.75, 1],
+                        }}
+                    >
+                        <GlobeIcon className="w-5 h-5 cursor-pointer stroke-black dark:stroke-white" />
+                    </motion.div>
+                </div>
+
+                <motion.p
+                    className="text-black cursor-default selection:bg-transparent dark:text-white"
+                    variants={themeLabelVarient}
+                    animate={theme === "light" ? "light" : "dark"}
+                    initial={{ opacity: 1 }}
+                >
+                    {/*  theme initialized, else know OS preference */}
+                    {theme ? theme : "System"}
+                </motion.p>
+            </div>
         ) : null
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,33 +2,15 @@ import "../styles/globals.css";
 import * as React from "react";
 import type { AppProps } from "next/app";
 import Header from "../components/Header/header";
-import DarkMode from "../components/DarkMode/DarkMode";
-
-// theme context
-export const ThemeContext = React.createContext({
-    theme: "placeholder",
-    setTheme: (string: string) => {},
-});
-
-// game context
-export const GameContext = React.createContext({
-    opponentHandler: (value: string, myID: string) => {},
-    whoIwantToPlay: "",
-    whoRequestMe: [""],
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const { theme, setTheme } = DarkMode();
-
     return (
         // wrap everything inside of contexts to be used later
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            <div className="h-screen overflow-hidden bg-white dark:bg-black">
-                {/* render header component and all other components */}
-                <Header />
-                <Component {...pageProps} />
-            </div>
-        </ThemeContext.Provider>
+        <main className="h-screen overflow-hidden bg-white dark:bg-black">
+            {/* render header component and all other components */}
+            <Header />
+            <Component {...pageProps} />
+        </main>
     );
 }
 
