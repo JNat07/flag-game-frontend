0d76a3fcb3416bfc5caccb2dd7be1b46ff8e1a17
Justin Morris <jnatmorris@gmail.com>
2022-04-02 12:42:19 +0200

    Add background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import "../styles/globals.css";
 import * as React from "react";
 import type { AppProps } from "next/app";
 import Header from "../components/Header/header";
+import Footer from "../components/Footer/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         // wrap everything inside of contexts to be used later
-        <main className="h-screen overflow-hidden bg-white dark:bg-black">
+        <main className="h-screen overflow-hidden bg-center bg-blueprint dark:bg-blueprintDark">
             {/* render header component and all other components */}
             <Header />
             <Component {...pageProps} />
+            <Footer />
         </main>
     );
 }
