e52ea2d516af54756b4c9baac97dbacdecabc2b6
Justin Morris <jnatmorris@gmail.com>
2022-04-07 22:45:25 +0200

    cleanup

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -15,44 +15,26 @@ const Header: React.FC = () => {
     }, []);
 
     return (
-        <div className="m-0">
-            <header className="relative inset-x-0 top-0 z-20 prose prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
-                <div className="grid w-screen grid-cols-3 px-2 mt-3">
-                    {/* only show home button when not on menu */}
-                    {router.pathname === "/" ? (
-                        <AnimatePresence>
-                            <motion.div
-                                whileTap={{ scale: 0.9 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 1 }}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                            >
-                                <FlagGameText />
-                            </motion.div>
-                        </AnimatePresence>
-                    ) : (
-                        <AnimatePresence>
-                            <Link href={"/"} passHref>
-                                <motion.a
-                                    whileTap={{ scale: 0.8 }}
-                                    transition={{ duration: 0.5 }}
-                                    exit={{ opacity: 0 }}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                >
-                                    <ChevronLeftIcon className="w-6 h-6 lg:h-8 lg:w-8 " />
-                                </motion.a>
-                            </Link>
-                        </AnimatePresence>
-                    )}
-                    <div /> {/* middle col empty */}
-                    <div className="justify-self-end">
-                        <ThemeToggle pageLoaded={pageLoaded} />
-                    </div>
+        <header className="relative inset-x-0 top-0 z-20 prose prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
+            <div className="grid w-screen grid-cols-3 px-2 pt-3">
+                {/* only show home button when not on menu */}
+                {router.pathname === "/" ? (
+                    <FlagGameText />
+                ) : (
+                    <AnimatePresence>
+                        <Link href={"/"} passHref>
+                            <motion.a whileTap={{ scale: 0.8 }}>
+                                <ChevronLeftIcon className="w-6 h-6 lg:h-8 lg:w-8 " />
+                            </motion.a>
+                        </Link>
+                    </AnimatePresence>
+                )}
+                <div /> {/* middle col empty */}
+                <div className="justify-self-end">
+                    <ThemeToggle pageLoaded={pageLoaded} />
                 </div>
-            </header>
-        </div>
+            </div>
+        </header>
     );
 };
 
--- components/Index/Menu.tsx
+++ components/Index/Menu.tsx
@@ -1,57 +1,108 @@
 import * as React from "react";
 import type { NextPage } from "next";
-import { motion } from "framer-motion";
-import Link from "next/link";
+import { motion, Variants } from "framer-motion";
 import { UserIcon, UsersIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 const Menu: NextPage = () => {
-    return (
-        <div className="fixed left-0 w-screen h-screen bg-center top-1 ">
-            {/* <Background /> */}
-            <main className="relative z-20 flex">
-                <div className="m-auto">
-                    <div className=" prose mt-[34vh] rounded-sm px-2 pb-4 pt-2  prose-h1:m-0 prose-h4:m-0 ">
-                        <div className="p-3 space-y-7">
-                            <div className="flex justify-center">
-                                <Link
-                                    href={"/singlePlayer"}
-                                    passHref
-                                    as="/singleplayer"
-                                >
-                                    <a className="no-underline rounded-md select-none">
-                                        <motion.div
-                                            whileTap={{ scale: 0.97 }}
-                                            className="flex items-center justify-center space-x-1 lg:space-x-2"
-                                        >
-                                            <UserIcon className="mb-1.5 h-8 w-8 text-black dark:text-white lg:h-14 lg:w-14" />
+    interface Props {
+        runAnimation: boolean;
+        endAnimation: boolean;
+        whereToSend: string;
+    }
+    const [click, setClick] = React.useState<Props>({
+        runAnimation: false,
+        endAnimation: false,
+        whereToSend: "",
+    });
 
-                                            <h4 className="text-3xl text-black dark:text-white">
-                                                Singleplayer
-                                            </h4>
-                                        </motion.div>
-                                    </a>
-                                </Link>
+    const router = useRouter();
+
+    const variants: Variants = {
+        animateVoid: {
+            scale: 100,
+            transition: {
+                duration: 1.5,
+            },
+        },
+        spotlight: {
+            x: [25, 0, -25, 0, 25],
+            y: [-20, -10, -5, -10, -20],
+
+            transition: {
+                repeat: Infinity,
+                duration: 10,
+            },
+        },
+    };
+
+    return (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-center">
+            <div className="absolute right-3">
+                <div className="h-[40vh] w-2 rounded-full bg-[#654321]" />
+            </div>
+            <main className="relative z-20 flex top-1">
+                <div className="m-auto">
+                    <div className="prose mt-[34vh] px-2 pb-4 pt-2 prose-h1:m-0 prose-h4:m-0">
+                        <div className="relative z-0">
+                            <motion.div
+                                variants={variants}
+                                className={
+                                    click.runAnimation
+                                        ? "absolute h-48 w-48 rounded-full bg-black opacity-100"
+                                        : "absolute h-48 w-48 rounded-full bg-gradient-to-r from-yellow-500  to-yellow-300 opacity-[0.35]"
+                                }
+                                animate={
+                                    click.runAnimation
+                                        ? "animateVoid"
+                                        : "spotlight"
+                                }
+                                onAnimationComplete={(definition) =>
+                                    definition === "animateVoid"
+                                        ? router.push(click.whereToSend)
+                                        : null
+                                }
+                            />
+                        </div>
+                        <div className="z-40 p-3 space-y-7">
+                            <div className="flex justify-center">
+                                <motion.div
+                                    whileTap={{ scale: 0.97 }}
+                                    className="flex items-center justify-center space-x-1 lg:space-x-2"
+                                    onClick={() =>
+                                        setClick({
+                                            runAnimation: true,
+                                            endAnimation: false,
+                                            whereToSend: "/singlePlayer",
+                                        })
+                                    }
+                                >
+                                    <UserIcon className="mb-1.5 h-8 w-8 text-black dark:text-white lg:h-14 lg:w-14" />
+
+                                    <h4 className="text-3xl text-black dark:text-white">
+                                        Singleplayer
+                                    </h4>
+                                </motion.div>
                             </div>
                             <div className="flex justify-center">
-                                <Link
-                                    href={"/multiPlayer"}
-                                    as="/multiplayer"
-                                    passHref
+                                <motion.div
+                                    whileTap={{
+                                        scale: 0.97,
+                                    }}
+                                    className="flex items-center justify-center space-x-2 lg:space-x-4"
+                                    onClick={() =>
+                                        setClick({
+                                            runAnimation: true,
+                                            endAnimation: false,
+                                            whereToSend: "/multiplayer",
+                                        })
+                                    }
                                 >
-                                    <a className="no-underline rounded-md select-none">
-                                        <motion.div
-                                            whileTap={{
-                                                scale: 0.97,
-                                            }}
-                                            className="flex items-center justify-center space-x-2 lg:space-x-4"
-                                        >
-                                            <UsersIcon className="mb-1.5 h-8 w-8 text-black dark:text-white lg:h-14 lg:w-14" />
-                                            <h4 className="text-3xl text-black dark:text-white">
-                                                Multiplayer
-                                            </h4>
-                                        </motion.div>
-                                    </a>
-                                </Link>
+                                    <UsersIcon className="mb-1.5 h-8 w-8 text-black dark:text-white lg:h-14 lg:w-14" />
+                                    <h4 className="text-3xl text-black dark:text-white">
+                                        Multiplayer
+                                    </h4>
+                                </motion.div>
                             </div>
                         </div>
                     </div>
