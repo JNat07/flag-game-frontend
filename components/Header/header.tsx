593480fe82261b37e533868914d1a4867d3e5f0b
Justin Morris <jnatmorris@gmail.com>
2022-04-07 17:58:48 +0200

    update header

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -18,36 +18,34 @@ const Header: React.FC = () => {
         <div className="m-0">
             <header className="relative inset-x-0 top-0 z-20 prose prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
                 <div className="grid w-screen grid-cols-3 px-2 mt-3">
-                    <div className="flex justify-self-start">
-                        {/* only show home button when not on menu */}
-                        {router.pathname === "/" ? (
-                            <AnimatePresence>
-                                <motion.div
-                                    whileTap={{ scale: 0.9 }}
+                    {/* only show home button when not on menu */}
+                    {router.pathname === "/" ? (
+                        <AnimatePresence>
+                            <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 1 }}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                            >
+                                <FlagGameText />
+                            </motion.div>
+                        </AnimatePresence>
+                    ) : (
+                        <AnimatePresence>
+                            <Link href={"/"} passHref>
+                                <motion.a
+                                    whileTap={{ scale: 0.8 }}
+                                    transition={{ duration: 0.5 }}
                                     exit={{ opacity: 0 }}
-                                    transition={{ duration: 1 }}
                                     initial={{ opacity: 0 }}
                                     animate={{ opacity: 1 }}
                                 >
-                                    <FlagGameText />
-                                </motion.div>
-                            </AnimatePresence>
-                        ) : (
-                            <AnimatePresence>
-                                <Link href={"/"} passHref>
-                                    <motion.a
-                                        whileTap={{ scale: 0.8 }}
-                                        transition={{ duration: 0.5 }}
-                                        exit={{ opacity: 0 }}
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                    >
-                                        <ChevronLeftIcon className="w-6 h-6 lg:h-8 lg:w-8 " />
-                                    </motion.a>
-                                </Link>
-                            </AnimatePresence>
-                        )}
-                    </div>
+                                    <ChevronLeftIcon className="w-6 h-6 lg:h-8 lg:w-8 " />
+                                </motion.a>
+                            </Link>
+                        </AnimatePresence>
+                    )}
                     <div /> {/* middle col empty */}
                     <div className="justify-self-end">
                         <ThemeToggle pageLoaded={pageLoaded} />
