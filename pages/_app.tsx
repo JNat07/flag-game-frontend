4a217ed169fb7730949b58ceaaa54d84f54045cf
Justin Morris <jnatmorris@gmail.com>
2022-04-07 22:45:05 +0200

    update background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import Footer from "../components/Footer/Footer";
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         // wrap everything inside of contexts to be used later
-        <main className="h-screen overflow-hidden bg-white dark:bg-black">
+        <main className="to-gray-transparent h-screen overflow-hidden bg-black bg-gradient-to-r from-red-800 bg-[length:45px_50px] dark:from-red-900 lg:bg-[length:70px_50px]">
             {/* render header component and all other components */}
             <Header />
             <Component {...pageProps} />
