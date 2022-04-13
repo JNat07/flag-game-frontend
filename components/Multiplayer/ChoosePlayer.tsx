657a43849237777d73f8aab854c3c4a14e806183
Justin Morris <jnatmorris@gmail.com>
2022-04-13 13:33:39 +0200

    Add title in head

--- components/Multiplayer/ChoosePlayer.tsx
+++ components/Multiplayer/ChoosePlayer.tsx
@@ -4,6 +4,7 @@ import { createAvatar } from "@dicebear/avatars";
 import * as style from "@dicebear/pixel-art";
 import { ChoosePlayerProps } from "../types";
 import Image from "next/image";
+import Head from "next/head";
 
 const ChoosePlayer: React.FC<ChoosePlayerProps> = ({
     playersReady,
@@ -20,105 +21,111 @@ const ChoosePlayer: React.FC<ChoosePlayerProps> = ({
     };
 
     return (
-        <div className="prose mt-[5vh] px-2 prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
-            <h2 className="text-center underline">
-                {playersReady.length >= 1
-                    ? `Online Players : ${playersReady.length}`
-                    : "Online Players"}
-            </h2>
-            <div className="grid grid-cols-2 items-center gap-y-2 gap-x-3">
-                {playersReady.length >= 1
-                    ? playersReady.map((elem, index) => {
-                          const { name, id } = elem;
+        <>
+            <Head>
+                <title>The Flag Game | Chooser Opponent</title>
+            </Head>
+            <div className="prose mt-[5vh] px-2 prose-h3:m-0 prose-p:m-0 prose-img:m-0 dark:prose-invert">
+                <h2 className="text-center underline">
+                    {playersReady.length >= 1
+                        ? `Online Players : ${playersReady.length}`
+                        : "Online Players"}
+                </h2>
+                <div className="grid items-center grid-cols-2 gap-y-2 gap-x-3">
+                    {playersReady.length >= 1
+                        ? playersReady.map((elem, index) => {
+                              const { name, id } = elem;
 
-                          if (myInfo.myID !== id)
-                              return (
-                                  <div
-                                      onClick={() =>
-                                          opponentHandler(id, myInfo.myID)
-                                      }
-                                      key={index}
-                                      className="cursor-pointer rounded-md ring-1 ring-black dark:ring-gray-400"
-                                  >
-                                      <motion.div
-                                          whileTap={{
-                                              scale: 0.95,
-                                          }}
-                                          className="w-full shadow-sm"
+                              if (myInfo.myID !== id)
+                                  return (
+                                      <div
+                                          onClick={() =>
+                                              opponentHandler(id, myInfo.myID)
+                                          }
+                                          key={index}
+                                          className="rounded-md cursor-pointer ring-1 ring-black dark:ring-gray-400"
                                       >
-                                          <div className="rounded-t-md bg-gray-500 selection:bg-transparent dark:bg-gray-800">
-                                              <AnimatePresence>
-                                                  {whoIwantToPlay === id && (
-                                                      <motion.div
-                                                          animate={{
-                                                              height: "auto",
-                                                              opacity: 1,
-                                                          }}
-                                                          initial={{
-                                                              height: 0,
-                                                              opacity: 0,
-                                                          }}
-                                                          exit={{
-                                                              height: 0,
-                                                              opacity: 0,
-                                                          }}
-                                                          className="overflow-hidden rounded-t-md bg-orange-600 pl-1 "
-                                                      >
-                                                          <p className="text-center text-base text-white">
-                                                              Requested
-                                                          </p>
-                                                      </motion.div>
-                                                  )}
-                                              </AnimatePresence>
+                                          <motion.div
+                                              whileTap={{
+                                                  scale: 0.95,
+                                              }}
+                                              className="w-full shadow-sm"
+                                          >
+                                              <div className="bg-gray-500 rounded-t-md selection:bg-transparent dark:bg-gray-800">
+                                                  <AnimatePresence>
+                                                      {whoIwantToPlay ===
+                                                          id && (
+                                                          <motion.div
+                                                              animate={{
+                                                                  height: "auto",
+                                                                  opacity: 1,
+                                                              }}
+                                                              initial={{
+                                                                  height: 0,
+                                                                  opacity: 0,
+                                                              }}
+                                                              exit={{
+                                                                  height: 0,
+                                                                  opacity: 0,
+                                                              }}
+                                                              className="pl-1 overflow-hidden bg-orange-600 rounded-t-md "
+                                                          >
+                                                              <p className="text-base text-center text-white">
+                                                                  Requested
+                                                              </p>
+                                                          </motion.div>
+                                                      )}
+                                                  </AnimatePresence>
 
-                                              <AnimatePresence>
-                                                  {whoRequestMe.includes(
-                                                      id
-                                                  ) && (
-                                                      <motion.div
-                                                          animate={{
-                                                              height: "auto",
-                                                              opacity: 1,
-                                                          }}
-                                                          initial={{
-                                                              height: 0,
-                                                              opacity: 0,
-                                                          }}
-                                                          exit={{
-                                                              height: 0,
-                                                              opacity: 0,
-                                                          }}
-                                                          className="overflow-hidden rounded-t-md bg-green-600 pl-1 "
-                                                      >
-                                                          <p className="text-center text-base text-white">
-                                                              Requesting You
-                                                          </p>
-                                                      </motion.div>
-                                                  )}
-                                              </AnimatePresence>
+                                                  <AnimatePresence>
+                                                      {whoRequestMe.includes(
+                                                          id
+                                                      ) && (
+                                                          <motion.div
+                                                              animate={{
+                                                                  height: "auto",
+                                                                  opacity: 1,
+                                                              }}
+                                                              initial={{
+                                                                  height: 0,
+                                                                  opacity: 0,
+                                                              }}
+                                                              exit={{
+                                                                  height: 0,
+                                                                  opacity: 0,
+                                                              }}
+                                                              className="pl-1 overflow-hidden bg-green-600 rounded-t-md "
+                                                          >
+                                                              <p className="text-base text-center text-white">
+                                                                  Requesting You
+                                                              </p>
+                                                          </motion.div>
+                                                      )}
+                                                  </AnimatePresence>
 
-                                              <div className="flex justify-center">
-                                                  <Image
-                                                      src={src(name)}
-                                                      width={120}
-                                                      height={120}
-                                                      alt="icon"
-                                                  />
+                                                  <div className="flex justify-center">
+                                                      <Image
+                                                          src={src(name)}
+                                                          width={120}
+                                                          height={120}
+                                                          alt="icon"
+                                                      />
+                                                  </div>
                                               </div>
-                                          </div>
 
-                                          <div>
-                                              <h3 className="break-all rounded-b-md bg-gray-300 text-center selection:bg-transparent dark:bg-slate-700/90">
-                                                  {name}
-                                              </h3>
-                                          </div>
-                                      </motion.div>
-                                  </div>
-                              );
-                      })
-                    : "none"}
+                                              <div>
+                                                  <h3 className="text-center break-all bg-gray-300 rounded-b-md selection:bg-transparent dark:bg-slate-700/90">
+                                                      {name}
+                                                  </h3>
+                                              </div>
+                                          </motion.div>
+                                      </div>
+                                  );
+                          })
+                        : "none"}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- components/Multiplayer/chooseName.tsx
+++ components/Multiplayer/chooseName.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { ChooseNameProps } from "../types";
 import { names } from "../randomInfo";
+import Head from "next/head";
 import avatarGenerator from "../AvatarGenerator";
 
 const ChooseName: React.FC<ChooseNameProps> = ({
@@ -22,74 +23,81 @@ const ChooseName: React.FC<ChooseNameProps> = ({
     };
 
     return (
-        <div className="relative  m-4 mt-[7vh]">
-            <div className="flex justify-center lg:w-screen">
-                <div className="p-4 prose rounded-lg shadow-inner prose-img:m-0 dark:prose-invert lg:px-10">
-                    <div className="space-y-5 ">
-                        <div className="mx-2 space-y-1">
-                            <h2 className="m-0 text-3xl text-center">
-                                Choose Your Name
-                            </h2>
-                            <hr className="dark:opacity-0" />
-                        </div>
+        <>
+            <Head>
+                <title>The Flag Game | Choose Name</title>
+            </Head>
+            <div className="relative  m-4 mt-[7vh]">
+                <div className="flex justify-center lg:w-screen">
+                    <div className="p-4 prose rounded-lg shadow-inner prose-img:m-0 dark:prose-invert lg:px-10">
+                        <div className="space-y-5 ">
+                            <div className="mx-2 space-y-1">
+                                <h2 className="m-0 text-3xl text-center">
+                                    Choose Your Name
+                                </h2>
+                                <hr className="dark:opacity-0" />
+                            </div>
 
-                        <div className="flex justify-center ">
-                            <div className="space-y-4">
-                                <div className="flex space-x-2">
-                                    <img
-                                        className="self-end w-16 h-16 rounded-lg bg-slate-600/80 selection:bg-transparent dark:bg-slate-600 dark:opacity-90"
-                                        src={avatarGenerator(myInfo.myName)}
-                                        alt="name_icon"
-                                    />
-
-                                    <div className="flex space-x-1 self-end rounded-md bg-gray-200 py-0.5 pl-1 dark:bg-gray-300 dark:ring-gray-100">
-                                        <motion.svg
-                                            onClick={() => randomName()}
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="currentColor"
-                                            className="self-center w-8 h-8 section:bg-transparent dark:invert"
-                                            whileTap={{ scale: 0.9 }}
-                                            viewBox="0 0 16 16"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"
-                                            />
-                                            <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z" />
-                                        </motion.svg>
-
-                                        <input
-                                            onChange={(e) =>
-                                                HandleSetName(e.target.value)
-                                            }
-                                            placeholder="Username"
-                                            className=" w-full self-end border-l-2 border-black border-transparent bg-gray-200 px-2  py-1.5 pr-1 text-xl outline-0 dark:bg-gray-300  dark:text-black"
-                                            value={myInfo.myName}
+                            <div className="flex justify-center ">
+                                <div className="space-y-4">
+                                    <div className="flex space-x-2">
+                                        <img
+                                            className="self-end w-16 h-16 rounded-lg bg-slate-600/80 selection:bg-transparent dark:bg-slate-600 dark:opacity-90"
+                                            src={avatarGenerator(myInfo.myName)}
+                                            alt="name_icon"
                                         />
-                                    </div>
-                                </div>
 
-                                <motion.button
-                                    whileTap={
-                                        myInfo.myName === ""
-                                            ? { scale: 1 }
-                                            : { scale: 0.98 }
-                                    }
-                                    onClick={() => chooseNameHandler()}
-                                    className={
-                                        myInfo.myName !== ""
-                                            ? "w-full rounded-md bg-blue-400 px-1.5 py-1 text-white shadow-md dark:bg-blue-500/90"
-                                            : "w-full rounded-md bg-blue-500 py-1 px-1.5 text-white opacity-60 shadow-md"
-                                    }
-                                >
-                                    Choose Name
-                                </motion.button>
+                                        <div className="flex space-x-1 self-end rounded-md bg-gray-200 py-0.5 pl-1 dark:bg-gray-300 dark:ring-gray-100">
+                                            <motion.svg
+                                                onClick={() => randomName()}
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
+                                                className="self-center w-8 h-8 section:bg-transparent dark:invert"
+                                                whileTap={{ scale: 0.9 }}
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"
+                                                />
+                                                <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z" />
+                                            </motion.svg>
+
+                                            <input
+                                                onChange={(e) =>
+                                                    HandleSetName(
+                                                        e.target.value
+                                                    )
+                                                }
+                                                placeholder="Username"
+                                                className=" w-full self-end border-l-2 border-black border-transparent bg-gray-200 px-2  py-1.5 pr-1 text-xl outline-0 dark:bg-gray-300  dark:text-black"
+                                                value={myInfo.myName}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <motion.button
+                                        whileTap={
+                                            myInfo.myName === ""
+                                                ? { scale: 1 }
+                                                : { scale: 0.98 }
+                                        }
+                                        onClick={() => chooseNameHandler()}
+                                        className={
+                                            myInfo.myName !== ""
+                                                ? "w-full rounded-md bg-blue-400 px-1.5 py-1 text-white shadow-md dark:bg-blue-500/90"
+                                                : "w-full rounded-md bg-blue-500 py-1 px-1.5 text-white opacity-60 shadow-md"
+                                        }
+                                    >
+                                        Choose Name
+                                    </motion.button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
