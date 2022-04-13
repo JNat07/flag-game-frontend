6778a529c7ba56722302ca7d9be67d80719005e1
Justin Morris <jnatmorris@gmail.com>
2022-04-13 14:15:15 +0200

    change folder case

--- pages/singlePlayer.tsx
+++ pages/singlePlayer.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import type { NextPage } from "next";
-import GameLogic from "../components/Game/GameLogic";
+import GameLogic from "../components/game/GameLogic";
 
-const SinglePlayer: NextPage = () => {
-    return <GameLogic />;
-};
+const SinglePlayer: NextPage = () => <GameLogic />;
 
 export default SinglePlayer;
