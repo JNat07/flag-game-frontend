9e29d6c347b51020ec7bcc01148684505e666059
Justin Morris <jnatmorris@gmail.com>
2022-04-13 16:51:10 +0200

    update paths

--- components/game/correctNames.tsx
+++ components/game/correctNames.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { countries } from "../FlagInfo/flaginfo";
+import { countries } from "../flagInfo/flaginfo";
 import { CorrectNamesType } from "../types";
 
 const CorrectNames: React.FC<CorrectNamesType> = ({
